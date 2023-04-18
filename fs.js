a080c7c63242261911b8ca3f044f92dc5af670ab
prajwal13579 <20951a05b1@iare.ac.in>
2023-04-18 14:16:42 +0530

    fs module

---
+++ fs.js
@@ -0,0 +1,36 @@
+const fs = require('fs')
+
+fs.writeFile(
+  'sample.txt',
+  'Hello world, this is awesome!!',
+  (err) => {
+    if (err) throw err
+    console.log('File created!')
+  }
+)
+
+fs.readFile(
+  'sample.txt', (err, data) => {
+    if (err) throw err
+    console.log(data.toString())
+  }
+)
+
+fs.appendFile('sample.txt', 'This is the fs module!!',
+  (err) => {
+    if (err) throw err
+    console.log('File appended!')
+  })
+
+fs.readFile(
+  'sample.txt', (err, data) => {
+    if (err) throw err
+    console.log(data.toString())
+  }
+)
+
+fs.rename('sample.txt', 'sample2.txt',
+  (err) => {
+    if (err) throw err
+    console.log('Name changed!')
+  })
